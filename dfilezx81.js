12dc2b0e8dbe869e172761a23e4945265ef76330
Charlie Robson <charlie.robson@b2m-solutions.com>
2018-03-27 10:00:55 +0100

    add cursor and initial text input

--- dfilezx81.js
+++ dfilezx81.js
@@ -1,13 +1,65 @@
 function dfilezx81() {
 
+    this.cx = 0;
+    this.cy = 0;
+
     this.preload = function () {
         this.font = loadImage('data/zx81.png');
         this.bg = createGraphics(256, 192);
         this.dfilemem = new ArrayBuffer(32 * 24);
         this.dfile = new Uint8Array(this.dfilemem);
-        this.dfile[100] = 12;
-        this.dfile[80] = 22;
-        this.dfile[300] = 135;
+    }
+
+    this.char = function (charcode) {
+        glyph = createImage(8, 8);
+        let fo = charcode > 127 ? 512 : 0;
+        charcode &= 127;
+        glyph.copy(this.font, 0, charcode * 8 + fo, 8, 8, 0, 0, 8, 8);
+        return glyph;
+    }
+
+    this.a2z = function (cc) {
+        let zeddycs = " ??????????\"£$:?()><=+-*/;,.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        let i = zeddycs.indexOf(cc);
+        if (i == -1) {
+            i = 15;
+        }
+        return i;
+    }
+
+    this.cursorRight = function () {
+        this.cx += 1;
+        if (this.cx > 31) {
+            this.cx = 0;
+            this.cursorDown();
+        }
+    }
+
+    this.cursorLeft = function () {
+        this.cx -= 1;
+        if (this.cx < 0) {
+            this.cx = 31;
+            this.cursorUp();
+        }
+    }
+
+    this.cursorDown = function () {
+        this.cy += 1;
+        if (this.cy > 24) {
+            this.cy = 0;
+        }
+    }
+
+    this.cursorUp = function () {
+        this.cy -= 1;
+        if (this.cy < 0) {
+            this.cy = 23;
+        }
+    }
+
+    this.rst10_ascii = function (cc) {
+        this.dfile[this.cx + 32 * this.cy] = this.a2z(cc);
+        this.cursorRight();
     }
 
     this.render = function (target) {
@@ -33,6 +85,6 @@ function dfilezx81() {
             }
         }
 
-        target.copy(this.bg, 0,0,256,192,0,0,256,192);
+        target.copy(this.bg, 0, 0, 256, 192, 0, 0, 256, 192);
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,19 +1,67 @@
 var dfile;
 var imgtarget;
+var imgLcursor;
+var dfilePanel;
 
-function preload(){
+function preload() {
   dfile = new dfilezx81();
   dfile.preload();
 }
 
 function setup() {
   createCanvas(800, 600);
-  imgtarget = createImage(256,192);
+  imgtarget = createImage(256, 192);
+  imgLcursor = dfile.char(0xb1);
   noSmooth();
+
+  dfilePanel = new dfilePanel_t();
 }
 
 function draw() {
   background(128);
   dfile.render(imgtarget);
   image(imgtarget, 0, 0, 512, 384);
+
+  if ((millis() & 512) == 0) image(imgLcursor, dfile.cx * 16, dfile.cy * 16, 16, 16);
+}
+
+function mouseClicked() {
+  dfilePanel.mouseClicked(0, 0);
+}
+
+function keyPressed() {
+  console.log("key: " + key.charCodeAt(0));
+  if (keyCode === BACKSPACE) {
+    dfile.cursorLeft();
+    dfile.rst10_ascii(" ");
+    dfile.cursorLeft();
+    return;
+  } else if (keyCode === UP_ARROW) {
+    dfile.cursorUp();
+    return;
+  } else if (keyCode === DOWN_ARROW) {
+    dfile.cursorDown();
+    return;
+  } else if (keyCode === LEFT_ARROW) {
+    dfile.cursorLeft();
+    return;
+  } else if (keyCode === RIGHT_ARROW) {
+    dfile.cursorRight();
+    return;
+  }
+
+  dfile.rst10_ascii(key);
+}
+
+function dfilePanel_t() {
+  this.mouseClicked = function (xo, yo) {
+    if (mouseX < xo || mouseX - xo >= 512 ||
+      mouseY < yo || mouseY - yo >= 384) {
+      return;
+    }
+
+    //this.deselect();
+    dfile.cx = (int)(mouseX / 16);
+    dfile.cy = (int)(mouseY / 16);
+  }
 }
